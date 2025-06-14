62fe7162ba850b35bd4d41853c2735869d92da29
Dazetazo <dazet@outlook.cl>
2025-06-14 12:57:52 -0400

    add 60fps

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+   
+    void Start()
+    {
+        //Forzar la aplicacion que se ejecute a 60 FPS
+        Application.targetFrameRate = 60;
+    }
+
+}
